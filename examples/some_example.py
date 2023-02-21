57cf980b2d8ff35107e522dc7a1cbacdf996869e
m-ksi <maximtimof194@gmail.com>
2023-02-21 23:17:32 +0300

    variables as attributes for Domain class, memberships as decorators, proper dunders

--- examples/some_example.py
+++ examples/some_example.py
@@ -1,12 +1,12 @@
 import time
-
-
 import src.fuzzyops as fo
 
-d = fo.Domain(0, 500, name='temperature (K)')
-d.add_linguistic('cold', fo.fuzzify.trapezoidalmf(d.x, -1, 0, 273, 280))
-d.add_linguistic('hot', fo.fuzzify.trapezoidalmf(d.x, 300, 310, 500, 500))
+d = fo.Domain((0, 500), name='temp')
+d.create_number(fo.fuzzify.trapezoidalmf, -1, 0, 273, 280, name='cold')
+d.create_number(fo.fuzzify.trapezoidalmf, 300, 310, 500, 500, name='hot')
 
-v0 = d.create_number('gauss', 30, 15)
-v1 = d.create_number('gauss', 30, 115)
+v0 = d.create_number('gauss', 30, 15, name='obs0')
+v1 = d.create_number('gauss', 30, 115, name='obs1')
+d.obs2 = v1 * 2
+d.obs3 = v0 + d.obs2 + 300
 d.plot()
--- src/fuzzyops/_domain.py
+++ src/fuzzyops/_domain.py
@@ -1,52 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from ._fuzzynumber import FuzzyNumber
-from typing import Callable, Union
+from typing import Callable, Union, Tuple, List
 from .fuzzify.mf import memberships
+from inspect import signature
+
+RealNum = Union[float, int]
 
 
 class Domain:
     """Domain that represents a set of possible values of a number
 
     """
-    def __init__(self, start, end, step=0.1, name=None, method='minimax'):
-        self._x = np.arange(start, end, step)
-        self.step = step
+    def __init__(self, fset: Union[Tuple[RealNum, RealNum], Tuple[RealNum, RealNum, RealNum], np.ndarray],
+                 name: str = None, method: str = 'minimax'):
+        assert (isinstance(fset, Tuple) and (len(fset) == 3 or len(fset) == 2)) or isinstance(fset, np.ndarray), \
+            'set bust be given as np.array or tuple with start, end, step values'
+        assert method == 'minimax' or method == 'prob', "Unknown method. Known methods are 'minmax' and 'prob'"
+        if len(fset) == 3:
+            start, end, step = fset
+            self._x = np.arange(start, end, step)
+            self.step = step
+        elif len(fset) == 2:
+            start, end = fset
+            self._x = np.arange(start, end, 1)
+            self.step = 1
+        else:
+            self._x = np.array(fset)
+            self.step = self._x[1] - self._x[0]
         self.name = name
-        self.ling_vars = {}
         self._method = method
-        self.vars = []
+        self._vars = {}
 
     @property
     def x(self):
         return self._x
 
-    def add_linguistic(self, name: str, mf: np.ndarray):
-        self.ling_vars[name] = mf
-
-    def create_number(self, membership: Union[str, Callable], *args: Union[int, float], to_vars=True):
+    def create_number(self, membership: Union[str, Callable], *args: RealNum, name: str = None):
+        assert isinstance(membership, str) or (isinstance(membership, Callable) and
+                                               len(args) == len(signature(membership).parameters))
         if isinstance(membership, str):
             membership = memberships[membership]
-        y = membership(self._x, *args)
-        f = FuzzyNumber(self, y, self._method)
-        if to_vars:
-            self.vars.append(f)
+        f = FuzzyNumber(self, membership(*args), self._method)
+        if name:
+            self.__setattr__(name, f)
         return f
 
-    def get_linguistic(self, name: str):
-        if name in self.ling_vars:
-            return FuzzyNumber(self, self.ling_vars[name], method=self._method)
+    def __setattr__(self, name, value):
+        if name in ['_x', 'step', 'name', '_method', '_vars']:
+            object.__setattr__(self, name, value)
         else:
-            raise KeyError
+            assert isinstance(name, str) and name not in self._vars, 'Name must be a unique string'
+            assert isinstance(value, FuzzyNumber), 'Value must be FuzzyNumber'
+            self._vars[name] = value
 
-    def plot(self, plot_vars: bool = True):
+    def __getattr__(self, name):
+        if name in self._vars:
+            return self._vars[name]
+        else:
+            raise AttributeError(f'{name} is not a variable in domain {self.name}')
+
+    def __delattr__(self, name):
+        if name in self._vars:
+            del self._vars[name]
+
+    def plot(self):
         _, ax = plt.subplots()
-        for name, y in self.ling_vars.items():
-            ax.plot(self.x, y, label=name)
 
-        if plot_vars:
-            for idx, f in enumerate(self.vars):
-                ax.plot(self.x, f.values, label=f'var{idx}')
+        for name, vals in self._vars.items():
+            ax.plot(self.x, vals, label=f'{name}')
 
         plt.title(self.name)
         ax.legend()
