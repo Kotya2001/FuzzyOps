af9a1ba03dad71f5ae533bea627f492b73c15a04
ilya Belozerov <iabelozerov@quasarpro.ru>
2023-04-16 20:41:22 +0300

    FIXES

--- src/fuzzyops/tests/run_tests.py
+++ src/fuzzyops/tests/run_tests.py
@@ -1,7 +1,6 @@
 from .fuzz_defuzzUnittest import TestSpeed, TestFuzzyNumber
 
 import unittest
-import argparse
 
 test_speed = unittest.TestLoader().loadTestsFromTestCase(TestSpeed)
 test_fuzzy_number = unittest.TestLoader().loadTestsFromTestCase(TestFuzzyNumber)
@@ -10,13 +9,9 @@ cases = {"speed": test_speed,
          "fn": test_fuzzy_number}
 
 
-def run_tests():
-    t = opt.type
-    unittest.TextTestRunner(verbosity=2).run(cases[t])
+def run_fn_test():
+    unittest.TextTestRunner(verbosity=2).run(cases["fn"])
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Test runner")
-    parser.add_argument("type", type=str, help="speed, fn")
-    opt = parser.parse_args()
-    run_tests()
+def run_speed_test():
+    unittest.TextTestRunner(verbosity=2).run(cases["speed"])
