9352117466e3d5c7f72989b4a57b7635ed9711d9
ilya Belozerov <iabelozerov@quasarpro.ru>
2025-02-05 21:42:14 +0300

    Start C++ wrappers ()

--- .history/ex2_20250205203007.cpp
+++
@@ -1,9 +0,0 @@
-#include <Python.h>
-#include <iostream>
-
-PyObject *import_module(const char *module_name) {
-    PyObject *pName = PyUnicode_FromString(module_name);
-    PyObject *pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-    return pModule;
-}
--- .history/ex_20250205161555.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include <Python.h>
-
-
